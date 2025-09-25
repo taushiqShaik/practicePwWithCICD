fa2a12aeb1bacb2d1824ddc58f474104f4276b59
taushiq <taushiq@gmail.com>
2025-09-25 16:43:40 +0530

    locator update of textBox

--- locators/textBox.locators.ts
+++ locators/textBox.locators.ts
@@ -1,8 +1,8 @@
 module.exports = {
-    elementsLink : "//h5[text()='Elements']",
-    textBox : "//span[text()='Text Box']",
-    fullNamePlaceHolder : "Full Name",
-    currentAddress : "Current Address",
-    permanentAddress : "//textarea[@id='permanentAddress']1",
-    submitButton : "submit"
-}
+  elementsLink: "//h5[text()='Elements']",
+  textBox: "//span[text()='Text Box']",
+  fullNamePlaceHolder: "Full Name",
+  currentAddress: "Current Address",
+  permanentAddress: "//textarea[@id='permanentAddress']",
+  submitButton: "submit",
+};
