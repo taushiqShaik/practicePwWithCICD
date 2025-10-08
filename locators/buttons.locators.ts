b7566a8c4b6f5abb1949ec035459db1f8df1af3d
taushiq <taushiq@gmail.com>
2025-10-08 07:05:24 +0530

    adding 4th file out of 4

---
+++ locators/buttons.locators.ts
@@ -0,0 +1,10 @@
+module.exports = {
+  buttons: "//span[text()='Buttons']",
+  doubleClickBtn: "#doubleClickBtn",
+  rightClickBtn: "//button[text()='Right Click Me']",
+  dynamicClickBtn: "//button[text()='Click Me']",
+  doubleClickMessage: "//p[text()='You have done a double click']",
+  rightClickMessage: "#rightClickMessage",
+  dynamicClickMessage: "#dynamicClickMessage",
+  buttonsHeader: "//h1[text()='Buttons']",
+};
